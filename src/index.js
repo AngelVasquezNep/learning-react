0219ad50dc52d2a9e82b88d15c2a96c0fbd188bc
Angel Vasquez <angel.vasquez.unam@gmail.com>
2021-11-28 18:18:57 -0600

    Working with props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+/**
+ * React component (JXS)
+ * class, function or const
+ * return
+ *  - null
+ *  - string
+ *  - number
+ *  - Otro componente de React
+ *  - HTML
+ *  - Array de cualquiera de los elementos anteriores
+ * 
+ * JXS -> Vanilla JS + HTML
+ */
+
+function Hi(props) {
+  return <h1>Hola</h1>
+}
+
+function SayHi(props) {
+  return <h2 title={props.name}>
+      Di mi nombre...{props.name} {props.home}
+  </h2>
+}
+
+function Nav() {
+  return <nav>NAVEGADOR</nav>
+}
+
+function Footer() {
+  return <footer>FOOTER</footer>
+}
+
+function Container() {
+  return (
+    <main>
+      <Nav />
+      <Hi />
+      <Hi />
+      <Hi />
+
+      <SayHi
+        name="Angelito"
+        home="México"
+      />
+      <SayHi name="Angelito Fake" />
+      <SayHi name="Angelito Panadero" />
+
+      <Footer />
+    </main>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>,
   document.getElementById('root')
 );
